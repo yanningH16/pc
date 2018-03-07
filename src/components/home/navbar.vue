9a936c12387f6aba1a8ce0b77c3172a8fd35063e
yanningH16 <632311638@qq.com>
2018-03-07 18:11:27 +0800

    优化

--- src/components/home/navbar.vue
+++ src/components/home/navbar.vue
@@ -16,29 +16,48 @@ export default {
   name: 'navBar',
   data () {
     return {
-      isActive: 0,
-      navArr: [
-        {
-          icon: 'el-icon-success',
-          header: '帐号管理',
-          router: 'account'
-        },
-        {
-          icon: 'el-icon-message',
-          header: '任务管理',
-          router: 'order'
-        },
-        {
-          icon: 'el-icon-more-outline',
-          header: '资金管理',
-          router: 'money'
-        },
-        {
-          icon: 'el-icon-upload',
-          header: '其他管理',
-          router: 'other'
-        }
-      ]
+      isActive: 0
+    }
+  },
+  computed: {
+    navArr: {
+      get () {
+        return [
+          {
+            icon: 'el-icon-success',
+            header: '帐号管理',
+            router: 'account'
+          },
+          {
+            icon: 'el-icon-message',
+            header: '任务管理',
+            router: 'order'
+          },
+          {
+            icon: 'el-icon-more-outline',
+            header: '资金管理',
+            router: 'money'
+          },
+          {
+            icon: 'el-icon-upload',
+            header: '其他管理',
+            router: 'other'
+          }
+        ]
+      }
+    }
+  },
+  created () {
+    let activeRouter = this.$route.path
+    console.log(activeRouter)
+    if (activeRouter.indexOf('account') !== -1) {
+      this.isActive = 0
+    } else if (activeRouter.indexOf('order') !== -1) {
+      this.isActive = 1
+    } else if (activeRouter.indexOf('money') !== -1) {
+      this.isActive = 2
+    } else if (activeRouter.indexOf('other') !== -1) {
+      this.isActive = 3
     }
   },
   methods: {
